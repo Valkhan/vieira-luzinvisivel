0f7dfd7697688e62eb331f9c42362e78d92b5e63
Paulo Lima <paulo.lima@valkhan.com.br>
2026-08-05 21:50:21 -0300

    Remoção de menções de tapume

--- js/contact-carousel.js
+++ js/contact-carousel.js
@@ -1,14 +1,14 @@
 (() => {
   const imagens = [
-    'img/fundos/ali_tapume_etereo_01.jpg',
-    'img/fundos/ali_tapume_etereo_02.jpg',
-    'img/fundos/ali_tapume_etereo_03.jpg',
-    'img/fundos/ali_tapume_mineral_01.jpg',
-    'img/fundos/ali_tapume_mineral_02.jpg',
-    'img/fundos/ali_tapume_mineral_03.jpg',
-    'img/fundos/ali_tapume_pele_01.jpg',
-    'img/fundos/ali_tapume_pele_02.jpg',
-    'img/fundos/ali_tapume_pele_03.jpg',
+    'img/fundos/a_luz_invisivel_etereo_01.jpg',
+    'img/fundos/a_luz_invisivel_etereo_02.jpg',
+    'img/fundos/a_luz_invisivel_etereo_03.jpg',
+    'img/fundos/a_luz_invisivel_mineral_01.jpg',
+    'img/fundos/a_luz_invisivel_mineral_02.jpg',
+    'img/fundos/a_luz_invisivel_mineral_03.jpg',
+    'img/fundos/a_luz_invisivel_pele_01.jpg',
+    'img/fundos/a_luz_invisivel_pele_02.jpg',
+    'img/fundos/a_luz_invisivel_pele_03.jpg',
   ];
 
   criarCarrosselDeFundo({
